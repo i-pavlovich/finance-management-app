58ae9a1e0a618af11981c6bd7fc1cbd8c9784b28
Ivan Pavlovich <ivan.pavlovich.business@gmail.com>
2024-02-05 23:27:43 +0300

    feat: add account creation through signals

--- src/finance_manager/apps.py
+++ src/finance_manager/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FinanceManagerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "finance_manager"
+
+    def ready(self) -> None:
+        import finance_manager.signals  # noqa
